817036c0511ab4e861843951148b7396e44b41aa
“hamsamuxumed” <hamsamuxumed3@gmail.com>
2022-04-16 19:58:55 +0100

    add fetch image url to compenont and render

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -19,10 +19,11 @@ export const Banner = ({ netflixOriginals }: Props) => {
   console.log(movie)
   return (
     <>
-      <div className="relative top-0 left-0">
+      <div className="relative top-0 left-0 h-[95vh] w-screen">
         <Image
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           layout="fill"
+          objectFit="contain"
         />
       </div>
     </>
