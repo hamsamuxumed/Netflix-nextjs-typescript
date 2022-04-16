463a27354eac7b8e5a9c50ea67603c709f994774
“hamsamuxumed” <hamsamuxumed3@gmail.com>
2022-04-16 19:25:23 +0100

    add request of diff categories  and add types

---
+++ components/Banner.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import { Movie } from '../typings'
+
+interface Props {
+  netflixOriginals: Movie[]
+}
+
+export const Banner = ({ netflixOriginals }: Props) => {
+  return
+  ;<>
+    {/* <div>
+      <Image />
+  </div> */}
+  </>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Header } from '../components/index'
+import { Header, Banner } from '../components/index'
+import { Movie } from '../typings'
+import { requests } from '../utils/requests'
 
-const Home: NextPage = () => {
+interface Props {
+  netflixOriginals: Movie[]
+  // trendingNow: Movie[]
+  // topRated: Movie[]
+  // actionMovies: Movie[]
+  // comedyMovies: Movie[]
+  // horrorMovies: Movie[]
+  // romanceMovies: Movie[]
+  // documentaries: Movie[]
+}
+
+const Home = ({ netflixOriginals }: Props) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -11,6 +23,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+      <Banner netflixOriginals={netflixOriginals} />
 
       <main>
         <section></section>
@@ -20,3 +33,15 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps = async () => {
+  const [netflixOriginals] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+  ])
+
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+    },
+  }
+}
