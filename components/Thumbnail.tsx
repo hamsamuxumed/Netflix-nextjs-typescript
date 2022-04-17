0cf7914b6e87d5782818ba7182dd1a8652653a38
“hamsamuxumed” <hamsamuxumed3@gmail.com>
2022-04-17 16:29:18 +0100

    remove scrollbar, add custom scroll bar, make left amd right icon functional

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -2,12 +2,13 @@ import Image from 'next/image'
 import { Movie } from '../typings'
 
 interface Props {
+  //movie: Movie | DocumentData
   movie: Movie
 }
 
 export const Thumbnail = ({ movie }: Props) => {
   return (
-    <div className="relative h-28 min-w-[180px] ">
+    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105 ">
       <Image
         className="rounded-sm object-cover md:rounded"
         src={`https://image.tmdb.org/t/p/w500${
