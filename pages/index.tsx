9729d67b5d42476c8973b6f92db2339eee1029c4
“hamsamuxumed” <hamsamuxumed3@gmail.com>
2022-04-17 16:55:19 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ const Home = ({
 }: Props) => {
   console.log(netflixOriginals)
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
         <title>Netflix - By Hamsa</title>
         <link rel="icon" href="/favicon.ico" />
