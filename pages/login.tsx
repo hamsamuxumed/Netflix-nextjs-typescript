cbb0ad0d3233cbb7cf9ebf574cc872457eae3f7b
“hamsamuxumed” <hamsamuxumed3@gmail.com>
2022-08-02 15:31:45 +0100

    add react hook forms to login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+type Inputs = {
+  email: string
+  password: string
+}
 
 function login() {
+  const [login, setLogin] = useState(false)
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
@@ -23,19 +40,45 @@ function login() {
         className="absolute top-4 left-4 cursor-pointer object-contain md:left-10 md:top-6"
       />
 
-      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+      >
         <h1 className="text-4xl font-semibold">Sign In</h1>
         <div className="space-y-4">
           <label className="inline-block w-full">
-            <input type="email" placeholder="Email" className="input" />
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              {...register('email', { required: true })}
+            />
+            {errors.email && <span className="">This field is required</span>}
           </label>
           <label className=" inline-block w-full">
-            <input type="password" placeholder="Password" className="input" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="input"
+              {...register('password', { required: true, minLength: 5 })}
+            />
+            {errors.password && (
+              <span>Password needs to be at least 5 characters</span>
+            )}
           </label>
         </div>
-        <button>Sign In</button>
-        <div>
-          <button>Sign Up Now</button>
+        <button
+          type="submit"
+          className="w-full rounded bg-[#e50914] py-3 font-semibold "
+        >
+          Sign In
+        </button>
+        <div className="text-[gray]">
+          New to Netflix?{' '}
+          <button type="submit" className="text-white hover:underline">
+            {' '}
+            Sign Up Now
+          </button>
         </div>
       </form>
     </div>
