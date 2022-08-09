bb84bd635414e23deb049440763ed568c23ab030
“hamsamuxumed” <hamsamuxumed3@gmail.com>
2022-08-09 15:05:57 +0100

    add react player and fetch trailer from API

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,9 +1,46 @@
+import { XIcon } from '@heroicons/react/solid'
 import MuiModal from '@mui/material/Modal'
+import { useEffect, useState } from 'react'
+import ReactPlayer from 'react-player/lazy'
 import { useRecoilState } from 'recoil'
-import { modalState } from '../atoms/modalAtom'
+import { modalState, movieState } from '../atoms/modalAtom'
+import { Element, Genre } from '../typings'
 
 export const Modal = () => {
   const [showModal, setShowModal] = useRecoilState(modalState)
+
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+  const [trailer, setTrailer] = useState('')
+  const [genres, setGenres] = useState<Genre[]>([])
+
+  useEffect(() => {
+    if (!currentMovie) return
+
+    async function fetchMovie() {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/${
+          currentMovie?.media_type === 'tv' ? 'tv' : 'movie'
+        }/${currentMovie?.id}?api_key=${
+          process.env.NEXT_PUBLIC_API_KEY
+        }&language=en-US&append_to_response=videos`
+      )
+        .then((resp) => resp.json())
+        .catch((err) => err.message)
+
+      if (data?.videos) {
+        const index = data?.videos.results.findIndex(
+          (element: Element) => element.type === 'Trailer'
+        )
+        setTrailer(data?.videos.results[index]?.key)
+      }
+
+      if (data?.genres) {
+        setGenres(data.genres)
+      }
+    }
+
+    fetchMovie()
+  }, [currentMovie])
 
   const handleClose = () => {
     setShowModal(false)
@@ -11,7 +48,24 @@ export const Modal = () => {
 
   return (
     <MuiModal open={showModal} onClose={handleClose}>
-      <>The modal is working!</>
+      <>
+        <button
+          onClick={handleClose}
+          className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
+        >
+          <XIcon className="h-6 w-6" />
+        </button>
+
+        <div>
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${trailer}'`}
+            width="100%"
+            height="100%"
+            style={{ position: 'absolute', top: '0', left: '0' }}
+            playing
+          />
+        </div>
+      </>
     </MuiModal>
   )
 }
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image'
+import { useRecoilState } from 'recoil'
+import { modalState, movieState } from '../atoms/modalAtom'
 import { Movie } from '../typings'
 
 interface Props {
@@ -7,9 +9,18 @@ interface Props {
 }
 
 export const Thumbnail = ({ movie }: Props) => {
+  const [showModal, setShowModal] = useRecoilState(modalState)
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+
+  const handleClick = () => {
+    setCurrentMovie(movie)
+    setShowModal(true)
+  }
+
   return (
-    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105 ">
+    <div className="relative  h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
       <Image
+        onClick={() => handleClick()}
         className="rounded-sm object-cover md:rounded"
         src={`https://image.tmdb.org/t/p/w500${
           movie.backdrop_path || movie.poster_path
